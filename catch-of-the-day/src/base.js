159cd6719a7e1615ab49fd5d16c95c3d70bd35b0
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-07-30 00:14:03 +0100

    connect to firebase

---
+++ catch-of-the-day/src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+	apiKey: "AIzaSyAqYGvY1OTCByoPrOUCQdVCUdk8fvESD2w",
+  authDomain: "catch-of-the-day-trizoza.firebaseapp.com",
+  databaseURL: "https://catch-of-the-day-trizoza.firebaseio.com"
+});
+
+export default base;
