1d28f1822700b5fb4b4bbfff7c0f76a3e5d2c877
Peter Trizuliak <peter.trizuliak@gmail.com>
2017-08-07 22:30:24 +0100

    deploy on github

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -10,17 +10,17 @@ class App extends React.Component {
 	constructor() {
 		super();
 		this.addFish = this.addFish.bind(this);
-		this.loadSamples = this.loadSamples.bind(this);
+		// this.loadSamples = this.loadSamples.bind(this);
 		this.addToOrder = this.addToOrder.bind(this);
 		this.updateFish = this.updateFish.bind(this);
 		this.removeFish = this.removeFish.bind(this);
 		this.removeFromOrder = this.removeFromOrder.bind(this);
-
-		this.state = {
-			fishes: {},
-			order: {}
-		};
 	}
+
+	state = {
+		fishes: {},
+		order: {}
+	};
 
 	componentWillMount() {
 		// runs right before the <App> is rendered
@@ -46,28 +46,28 @@ class App extends React.Component {
 		localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
 	}
 
-	loadSamples() {
+	loadSamples = () => {
 		this.setState({ fishes: sampleFishes })
-	}
+	};
 
 	addFish(fish) {
 		const fishes = {...this.state.fishes};
 		const timeStamp = Date.now();
 		fishes[`fish-${timeStamp}`] = fish;
 		this.setState({ fishes: fishes});
-	}
+	};
 
 	updateFish(key, updatedFish) {
 		const fishes = {...this.state.fishes};
 		fishes[key] = updatedFish;
 		this.setState({ fishes });
-	}
+	};
 
-	removeFish(key) {
+	removeFish = (key) => {
 		const fishes = {...this.state.fishes};
 		fishes[key] = null;
 		this.setState({ fishes });
-	}
+	};
 
 	removeFromOrder(key) {
 		const order = {...this.state.order};
